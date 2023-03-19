021673d47961d17a5fea009d1f41783f197bfa42
S.Iman <simangali2002@gmail.com>
2023-03-19 10:25:30 +0600

    fix(save changes): linter

--- app/Models/User.php
+++ app/Models/User.php
@@ -11,7 +11,8 @@ use Laravel\Sanctum\HasApiTokens;
 /**
  * @mixin IdeHelperUser
  */
-class User extends Authenticatable { 
+class User extends Authenticatable
+{
     use HasApiTokens, HasFactory, Notifiable;
 
     /**
